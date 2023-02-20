217b8f5af375c4079e53d42ea15edfbc83be28d2
myUsername <adebayorgrace2017@gmail.com>
2023-02-20 06:43:31 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - A C program that prints exactly a line to a standard error
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful"\ - Dora Korpar, 2015-10-19\n" , stdout);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
