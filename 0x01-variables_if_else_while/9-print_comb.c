b15d7ce14bf5c9ba4f771192efbd128c11369d92
myUsername <adebayorgrace2017@gmail.com>
2023-02-20 21:12:41 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,16 @@ int main(void)
 {
 	int i;
 
-        for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else 
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
-        return (0);
+	putchar('\n');
+	return (0);
 }
 
