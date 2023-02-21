488a03a87a90109c905c04712251ad0aa8b7da39
myUsername <adebayorgrace2017@gmail.com>
2023-02-20 18:48:30 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- *main - Entry point
- *Description: 'get the last digit of a number'
- *Return: 0 (Success)
+ * main - Entry point
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	int n;
-	int ld;
+        int n;
+        int m;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	ld = n % 10;
-	if (ld > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5", n, ld);
-	}
-	else if (ld == 0)
-	{
-		printf("Last digit of %d is %d and is 0", n, ld);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and less 6 and not 0", n, ld);
-	}
+        srand(time(0)); 
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        m = n % 10;
+        if (m > 5)
+                printf("Last digit of %d is %d and is greater than 5", n, m);
+	 else if (m == 0)
+                printf("Last digit of %d is %d and is 0", n, m);
+        else if (m < 6 && m != 0)
+               printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
+
+	printf("\n");
+
 	return (0);
 }	
+
+
+	
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,27 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.>
 /**
  * main - Entry point 
  * Description: 'Print the alphabets in lowercase and uppercase'
+ *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	int n = 97;
-	int m = 65;
+	int n;
+	int m;
 
-	while (n <= 122)
-	{
-		putchar(n);
-		n++;
-	}
-	while (m <= 90)
+        srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	m = n % 10;
+	if (m > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, m);
+	else if (m == 0)
+		printf("Last digit of %d is %d and is 0", n, m);
+	else if (m < 6 && m != 0)
+	       printf("Last digit og %d is %d and is less than 	
 	{
 		putchar(m);
 		m++;
