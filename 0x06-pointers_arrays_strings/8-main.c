3c18d6f8b9528e080c646597587f85374f9d92f2
myUsername <adebayorgrace2017@gmail.com>
2023-03-02 15:02:08 -0800

    More pointers arrays and strings

---
+++ 0x06-pointers_arrays_strings/8-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+	char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+	char *p;
+
+	p = rot13(s);
+	printf("%s", p);
+
